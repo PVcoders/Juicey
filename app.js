e09d3500c7124eb4d89d08b43153b209345e2eb6
Juicey <juicetin.kim@gmail.com>
2020-09-20 18:58:52 -0400

    removed stories

--- app.js
+++ app.js
@@ -232,9 +232,9 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.get("/stories", function(req, res) {
-  res.render("stories");
-});
+// app.get("/stories", function(req, res) {
+//   res.render("stories");
+// });
 
 app.get("/contact", function(req, res) {
   res.render("contact");
